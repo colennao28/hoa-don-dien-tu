52758428a495c2898ce10944f23dd51cc95a342e
Nguyen Tien <tryforfuture96@gmail.com>
2022-06-28 15:23:36 +0700

    add new code

--- hoa-don-dien-tu/Models/HoaDon.cs
+++ hoa-don-dien-tu/Models/HoaDon.cs
@@ -11,23 +11,23 @@ namespace hoa_don_dien_tu.Models
         public string nmmst { get; set; }
         public string nmdchi { get; set; }
         public string shdon { get; set; }
-        public DateTime ncma { get; set; } // ngay hop dong
+        public DateTime? nky { get; set; } // ngay hop dong
         public string nbten { get; set; }
         public string nbmst { get; set; }
         public string nbdchi { get; set; }
-        public float TSTSauThue { get; set; }
-        public float TongVAT { get; set; }
+        public float? TSTSauThue { get; set; }
+        public float? TongVAT { get; set; }
         public string LoaiTien { get; set; }
-        public float TiGia { get; set; }
+        public float? TiGia { get; set; }
         public string TenHangHoa { get; set; }
         public string DonViTinh { get; set; }
-        public int SoLuong { get; set; }
-        public float DonGia { get; set; }
+        public int? SoLuong { get; set; }
+        public float? DonGia { get; set; }
         public float? TiLeCK { get; set; }
-        public float SoTienCK { get; set; }
-        public float ThanhTien { get; set; }
-        public float ThueSuat { get; set; }
-        public float TienThueVAT { get; set; }
+        public float? SoTienCK { get; set; }
+        public float? ThanhTien { get; set; }
+        public float? ThueSuat { get; set; }
+        public float? TienThueVAT { get; set; }
         public string CheckUnique { get; set; }
     }
 }
--- hoa-don-dien-tu/Program.cs
+++ hoa-don-dien-tu/Program.cs
@@ -8,21 +8,26 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 
+
 namespace hoa_don_dien_tu
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             // read file
             var lines = File.ReadAllLines("config.txt");
             var token = lines[0].ToString().Trim();
             var startDate = lines[1].ToString().Trim();
             var endDate = lines[2].ToString().Trim();
             // lay tong hoa don
-            var url = $"https://hoadondientu.gdt.gov.vn:30000/query/invoices/sold?sort=tdlap:desc,khmshdon:asc,shdon:desc&size=1000000&search=tdlap=ge={startDate}T00:00:00;tdlap=le={endDate}T23:59:59";
-            
-            var danhsachhoadon = new List<HoaDon>();
+            var urlban = $"https://hoadondientu.gdt.gov.vn:30000/query/invoices/sold?sort=tdlap:desc,khmshdon:asc,shdon:desc&size=1000000&search=tdlap=ge={startDate}T00:00:00;tdlap=le={endDate}T23:59:59";
+            var urlmua = $"https://hoadondientu.gdt.gov.vn:30000/query/invoices/purchase?sort=tdlap:desc,khmshdon:asc,shdon:desc&size=1000000&search=tdlap=ge={startDate}T00:00:00;tdlap=le={endDate}T23:59:59";
+
+            var danhsachhoadonban = new List<HoaDon>();
+            var danhsachhoadonmua = new List<HoaDon>();
+            Console.WriteLine($"Đang lấy danh sách hóa đơn bán và mua ...");
 
             var firstTryCount = 0;
 
@@ -34,43 +39,55 @@ namespace hoa_don_dien_tu
                     {
                         //http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                         http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                        var response = http.GetAsync(url).GetAwaiter().GetResult();
 
-                        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                        // lay danh sach ban
+                        var responseban = http.GetAsync(urlban).GetAwaiter().GetResult();
+                        if (responseban.StatusCode != System.Net.HttpStatusCode.OK)
                         {
-                            throw new Exception($"Mã lỗi: {response.StatusCode} - {response.Content.ToString()}");
+                            throw new Exception($"Mã lỗi: {responseban.StatusCode} - {responseban.Content.ToString()}");
                         }
-                        var result = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
-                        var datas = result.datas;
+                        var resultban = JsonConvert.DeserializeObject<dynamic>(responseban.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                        var datasban = resultban.datas;
+                        danhsachhoadonban = JsonConvert.DeserializeObject<List<HoaDon>>(JsonConvert.SerializeObject(datasban));
 
-                        danhsachhoadon = JsonConvert.DeserializeObject<List<HoaDon>>(JsonConvert.SerializeObject(datas));
-                        Console.WriteLine($"Co tat ca {danhsachhoadon.Count} hoa don");
+                        // lay danh sach mua
+                        var responsemua = http.GetAsync(urlmua).GetAwaiter().GetResult();
+                        if (responsemua.StatusCode != System.Net.HttpStatusCode.OK)
+                        {
+                            throw new Exception($"Mã lỗi: {responsemua.StatusCode} - {responsemua.Content.ToString()}");
+                        }
+                        var resultmua = JsonConvert.DeserializeObject<dynamic>(responsemua.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                        var datasmua = resultmua.datas;
+                        danhsachhoadonmua = JsonConvert.DeserializeObject<List<HoaDon>>(JsonConvert.SerializeObject(datasmua));
+
+
+                        Console.WriteLine($"Có tất cả {danhsachhoadonban.Count} hóa đơn bán và {danhsachhoadonmua.Count} hóa đơn mua!");
                         break;
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
                     firstTryCount++;
                 }
             }
 
-            if(firstTryCount >= 3)
+            if (firstTryCount >= 3)
             {
-                Console.WriteLine("Xin lay lai token");
+                Console.WriteLine("Lỗi!!! Xin lấy lại token và chạy lại!");
                 Console.ReadKey();
                 return;
             }
             
 
-            Console.WriteLine("Dang lay chi tiet hoa don ...");
-            int index = 0;
-            foreach(var hoadon in danhsachhoadon)
+            Console.WriteLine("Đang lấy thông tin chi tiết hóa đơn ...");
+            int indexban = 0;
+            foreach(var hoadon in danhsachhoadonban)
             {
-                index++;
-                Console.WriteLine($"Dang lay chi tiet hoa don ... {index} / {danhsachhoadon.Count}");
+                indexban++;
+                Console.WriteLine($"Đang lấy thông tin chi tiết hóa đơn bán ... {indexban} / {danhsachhoadonban.Count}");
                 var detailUrl = $"https://hoadondientu.gdt.gov.vn:30000/query/invoices/detail?nbmst={hoadon.nbmst}&khhdon={hoadon.khhdon}&shdon={hoadon.shdon}&khmshdon=1";
                 var secondTryCount = 0;
-                while (secondTryCount < 5)
+                while (secondTryCount < 10)
                 {
                     try
                     {
@@ -108,66 +125,177 @@ namespace hoa_don_dien_tu
                     }
                 }
 
-                if (secondTryCount >= 5)
+                if (secondTryCount >= 10)
                 {
-                    Console.WriteLine("Xin lay lai token");
+                    Console.WriteLine("Lỗi!!! Xin lấy lại token và chạy lại!");
                     Console.ReadKey();
                     return;
                 }
             }
 
-            var workbook = new XLWorkbook();
-            IXLWorksheet workSheet = workbook.Worksheets.Add($"Hoa Don Dien Tu");
-            workSheet.Cell(1, 1).Value = "Tên người mua";
-            workSheet.Cell(1, 2).Value = "MST người mua";
-            workSheet.Cell(1, 3).Value = "Địa chỉ người mua";
-            workSheet.Cell(1, 4).Value = "Số HĐ";
-            workSheet.Cell(1, 5).Value = "Ngày HĐ";
-            workSheet.Cell(1, 6).Value = "Tên người bán";
-            workSheet.Cell(1, 7).Value = "MST Người bán";
-            workSheet.Cell(1, 8).Value = "Địa chỉ người bán";
-            workSheet.Cell(1, 9).Value = "Tổng số tiền sau thuế";
-            workSheet.Cell(1, 10).Value = "Tổng VAT";
-            workSheet.Cell(1, 11).Value = "Loại tiền";
-            workSheet.Cell(1, 12).Value = "Tỷ giá";
-            workSheet.Cell(1, 13).Value = "STT";
-            workSheet.Cell(1, 14).Value = "Tên hàng hóa dịch vụ";
-            workSheet.Cell(1, 15).Value = "Đơn vị tính";
-            workSheet.Cell(1, 16).Value = "Số lượng";
-            workSheet.Cell(1, 17).Value = "Đơn giá";
-            workSheet.Cell(1, 18).Value = "Tỷ lệ CK";
-            workSheet.Cell(1, 19).Value = "Số tiền Ck";
-            workSheet.Cell(1, 20).Value = "Thành tiền";
-            workSheet.Cell(1, 21).Value = "Thuế Suất";
-            workSheet.Cell(1, 22).Value = "Tiền thuế VAT";
-            workSheet.Cell(1, 23).Value = "Check Unique";
 
-            for (int i = 0; i < danhsachhoadon.Count; i++)
+            int indexmua = 0;
+            foreach (var hoadon in danhsachhoadonmua)
             {
-                var row = danhsachhoadon[i];
-                workSheet.Cell(2 + i, 1).Value = row.nmten;
-                workSheet.Cell(2 + i, 2).Value = row.nmmst;
-                workSheet.Cell(2 + i, 3).Value = row.nmdchi;
-                workSheet.Cell(2 + i, 4).Value = row.shdon;
-                workSheet.Cell(2 + i, 5).Value = row.ncma.ToString();
-                workSheet.Cell(2 + i, 6).Value = row.nbten;
-                workSheet.Cell(2 + i, 7).Value = row.nbmst;
-                workSheet.Cell(2 + i, 8).Value = row.nbdchi;
-                workSheet.Cell(2 + i, 9).Value = row.TSTSauThue.ToString();
-                workSheet.Cell(2 + i, 10).Value = row.TongVAT.ToString();
-                workSheet.Cell(2 + i, 11).Value = row.LoaiTien;
-                workSheet.Cell(2 + i, 12).Value = row.TiGia.ToString();
-                workSheet.Cell(2 + i, 13).Value = (i + 1).ToString();
-                workSheet.Cell(2 + i, 14).Value = row.TenHangHoa;
-                workSheet.Cell(2 + i, 15).Value = row.DonViTinh;
-                workSheet.Cell(2 + i, 16).Value = row.SoLuong.ToString();
-                workSheet.Cell(2 + i, 17).Value = row.DonGia.ToString();
-                workSheet.Cell(2 + i, 18).Value = row.TiLeCK == null ? "-" : row.TiLeCK.ToString();
-                workSheet.Cell(2 + i, 19).Value = row.SoTienCK.ToString();
-                workSheet.Cell(2 + i, 20).Value = row.ThanhTien.ToString();
-                workSheet.Cell(2 + i, 21).Value = row.ThueSuat.ToString();
-                workSheet.Cell(2 + i, 22).Value = row.TienThueVAT.ToString();
-                workSheet.Cell(2 + i, 23).Value = "";
+                indexmua++;
+                Console.WriteLine($"Đang lấy thông tin chi tiết hóa đơn mua ... {indexmua} / {danhsachhoadonmua.Count}");
+                var detailUrl = $"https://hoadondientu.gdt.gov.vn:30000/query/invoices/detail?nbmst={hoadon.nbmst}&khhdon={hoadon.khhdon}&shdon={hoadon.shdon}&khmshdon=1";
+                var secondTryCount = 0;
+                while (secondTryCount < 10)
+                {
+                    try
+                    {
+                        using (var http = new HttpClient())
+                        {
+                            //http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                            http.Timeout = TimeSpan.FromSeconds(5);
+                            var response = http.GetAsync(detailUrl).GetAwaiter().GetResult();
+
+                            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                            {
+                                throw new Exception($"Mã lỗi: {response.StatusCode} - {response.Content.ToString()}");
+                            }
+                            var result = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
+                            hoadon.TSTSauThue = float.Parse(result?.tgtttbso.ToString());
+                            hoadon.TongVAT = float.Parse(result?.tgtthue.ToString());
+                            hoadon.LoaiTien = result?.dvtte;
+                            hoadon.TiGia = result?.tgia;
+                            hoadon.TenHangHoa = result?.hdhhdvu[0]?.ten;
+                            hoadon.DonViTinh = result?.hdhhdvu[0]?.dvtinh;
+                            hoadon.SoLuong = result?.hdhhdvu[0]?.sluong;
+                            hoadon.DonGia = result?.hdhhdvu[0]?.dgia;
+                            hoadon.TiLeCK = result?.hdhhdvu[0]?.tlckhau;
+                            hoadon.ThanhTien = result?.hdhhdvu[0]?.thtien;
+                            hoadon.ThueSuat = result?.hdhhdvu[0]?.tsuat;
+                            hoadon.TienThueVAT = hoadon.TongVAT;
+                            break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        secondTryCount++;
+                    }
+                }
+
+                if (secondTryCount >= 10)
+                {
+                    Console.WriteLine("Lỗi!!! Xin lấy lại token và chạy lại!");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Lấy thông tin thành công => in ra file ...");
+            // ban
+            var workbook = new XLWorkbook();
+            IXLWorksheet workSheetBan = workbook.Worksheets.Add($"Hóa Đơn Bán");
+            workSheetBan.Cell(1, 1).Value = "Tên người mua";
+            workSheetBan.Cell(1, 2).Value = "MST người mua";
+            workSheetBan.Cell(1, 3).Value = "Địa chỉ người mua";
+            workSheetBan.Cell(1, 4).Value = "Số HĐ";
+            workSheetBan.Cell(1, 5).Value = "Ngày HĐ";
+            workSheetBan.Cell(1, 6).Value = "Tên người bán";
+            workSheetBan.Cell(1, 7).Value = "MST Người bán";
+            workSheetBan.Cell(1, 8).Value = "Địa chỉ người bán";
+            workSheetBan.Cell(1, 9).Value = "Tổng số tiền sau thuế";
+            workSheetBan.Cell(1, 10).Value = "Tổng VAT";
+            workSheetBan.Cell(1, 11).Value = "Loại tiền";
+            workSheetBan.Cell(1, 12).Value = "Tỷ giá";
+            workSheetBan.Cell(1, 13).Value = "STT";
+            workSheetBan.Cell(1, 14).Value = "Tên hàng hóa dịch vụ";
+            workSheetBan.Cell(1, 15).Value = "Đơn vị tính";
+            workSheetBan.Cell(1, 16).Value = "Số lượng";
+            workSheetBan.Cell(1, 17).Value = "Đơn giá";
+            workSheetBan.Cell(1, 18).Value = "Tỷ lệ CK";
+            workSheetBan.Cell(1, 19).Value = "Số tiền Ck";
+            workSheetBan.Cell(1, 20).Value = "Thành tiền";
+            workSheetBan.Cell(1, 21).Value = "Thuế Suất";
+            workSheetBan.Cell(1, 22).Value = "Tiền thuế VAT";
+            workSheetBan.Cell(1, 23).Value = "Check Unique";
+
+            for (int i = 0; i < danhsachhoadonban.Count; i++)
+            {
+                var row = danhsachhoadonban[i];
+                workSheetBan.Cell(2 + i, 1).Value = row.nmten;
+                workSheetBan.Cell(2 + i, 2).Value = row.nmmst;
+                workSheetBan.Cell(2 + i, 3).Value = row.nmdchi;
+                workSheetBan.Cell(2 + i, 4).Value = row.shdon;
+                workSheetBan.Cell(2 + i, 5).Value = row.nky.ToString();
+                workSheetBan.Cell(2 + i, 6).Value = row.nbten;
+                workSheetBan.Cell(2 + i, 7).Value = row.nbmst;
+                workSheetBan.Cell(2 + i, 8).Value = row.nbdchi;
+                workSheetBan.Cell(2 + i, 9).Value = row.TSTSauThue.ToString();
+                workSheetBan.Cell(2 + i, 10).Value = row.TongVAT.ToString();
+                workSheetBan.Cell(2 + i, 11).Value = row.LoaiTien;
+                workSheetBan.Cell(2 + i, 12).Value = row.TiGia.ToString();
+                workSheetBan.Cell(2 + i, 13).Value = (i + 1).ToString();
+                workSheetBan.Cell(2 + i, 14).Value = row.TenHangHoa;
+                workSheetBan.Cell(2 + i, 15).Value = row.DonViTinh;
+                workSheetBan.Cell(2 + i, 16).Value = row.SoLuong.ToString();
+                workSheetBan.Cell(2 + i, 17).Value = row.DonGia.ToString();
+                workSheetBan.Cell(2 + i, 18).Value = row.TiLeCK == null ? "-" : row.TiLeCK.ToString();
+                workSheetBan.Cell(2 + i, 19).Value = row.SoTienCK.ToString();
+                workSheetBan.Cell(2 + i, 20).Value = row.ThanhTien.ToString();
+                workSheetBan.Cell(2 + i, 21).Value = row.ThueSuat.ToString();
+                workSheetBan.Cell(2 + i, 22).Value = row.TienThueVAT.ToString();
+                workSheetBan.Cell(2 + i, 23).Value = "";
+            }
+
+
+            // mua
+            IXLWorksheet workbookMua = workbook.Worksheets.Add($"Hóa Đơn Mua");
+            workbookMua.Cell(1, 1).Value = "Tên người mua";
+            workbookMua.Cell(1, 2).Value = "MST người mua";
+            workbookMua.Cell(1, 3).Value = "Địa chỉ người mua";
+            workbookMua.Cell(1, 4).Value = "Số HĐ";
+            workbookMua.Cell(1, 5).Value = "Ngày HĐ";
+            workbookMua.Cell(1, 6).Value = "Tên người bán";
+            workbookMua.Cell(1, 7).Value = "MST Người bán";
+            workbookMua.Cell(1, 8).Value = "Địa chỉ người bán";
+            workbookMua.Cell(1, 9).Value = "Tổng số tiền sau thuế";
+            workbookMua.Cell(1, 10).Value = "Tổng VAT";
+            workbookMua.Cell(1, 11).Value = "Loại tiền";
+            workbookMua.Cell(1, 12).Value = "Tỷ giá";
+            workbookMua.Cell(1, 13).Value = "STT";
+            workbookMua.Cell(1, 14).Value = "Tên hàng hóa dịch vụ";
+            workbookMua.Cell(1, 15).Value = "Đơn vị tính";
+            workbookMua.Cell(1, 16).Value = "Số lượng";
+            workbookMua.Cell(1, 17).Value = "Đơn giá";
+            workbookMua.Cell(1, 18).Value = "Tỷ lệ CK";
+            workbookMua.Cell(1, 19).Value = "Số tiền Ck";
+            workbookMua.Cell(1, 20).Value = "Thành tiền";
+            workbookMua.Cell(1, 21).Value = "Thuế Suất";
+            workbookMua.Cell(1, 22).Value = "Tiền thuế VAT";
+            workbookMua.Cell(1, 23).Value = "Check Unique";
+
+            for (int i = 0; i < danhsachhoadonmua.Count; i++)
+            {
+                var row = danhsachhoadonmua[i];
+                workbookMua.Cell(2 + i, 1).Value = row.nmten;
+                workbookMua.Cell(2 + i, 2).Value = row.nmmst;
+                workbookMua.Cell(2 + i, 3).Value = row.nmdchi;
+                workbookMua.Cell(2 + i, 4).Value = row.shdon;
+                workbookMua.Cell(2 + i, 5).Value = row.nky.ToString();
+                workbookMua.Cell(2 + i, 6).Value = row.nbten;
+                workbookMua.Cell(2 + i, 7).Value = row.nbmst;
+                workbookMua.Cell(2 + i, 8).Value = row.nbdchi;
+                workbookMua.Cell(2 + i, 9).Value = row.TSTSauThue.ToString();
+                workbookMua.Cell(2 + i, 10).Value = row.TongVAT.ToString();
+                workbookMua.Cell(2 + i, 11).Value = row.LoaiTien;
+                workbookMua.Cell(2 + i, 12).Value = row.TiGia.ToString();
+                workbookMua.Cell(2 + i, 13).Value = (i + 1).ToString();
+                workbookMua.Cell(2 + i, 14).Value = row.TenHangHoa;
+                workbookMua.Cell(2 + i, 15).Value = row.DonViTinh;
+                workbookMua.Cell(2 + i, 16).Value = row.SoLuong.ToString();
+                workbookMua.Cell(2 + i, 17).Value = row.DonGia.ToString();
+                workbookMua.Cell(2 + i, 18).Value = row.TiLeCK == null ? "-" : row.TiLeCK.ToString();
+                workbookMua.Cell(2 + i, 19).Value = row.SoTienCK.ToString();
+                workbookMua.Cell(2 + i, 20).Value = row.ThanhTien.ToString();
+                workbookMua.Cell(2 + i, 21).Value = row.ThueSuat.ToString();
+                workbookMua.Cell(2 + i, 22).Value = row.TienThueVAT.ToString();
+                workbookMua.Cell(2 + i, 23).Value = "";
             }
 
             var filePath = Path.Combine(startDate.Replace("/", "-") + " đến " + endDate.Replace("/", "-") + "_" + Guid.NewGuid().ToString() + ".xlsx");
@@ -176,7 +304,8 @@ namespace hoa_don_dien_tu
                 workbook.SaveAs(stream);
             }
 
-            Thread.Sleep(100);
+            Console.WriteLine($"DONE!!!");
+            Console.ReadKey();
         }
     }
 }
